b0a5f5f3b7896eb247e9f91858c04671aa78975a
anusha <anushavubba@gmail.com>
2026-02-02 10:54:25 +0530

    added files

---
+++ functions.py
@@ -0,0 +1,104 @@
+# def add(a, b):
+#     print(a + b)
+
+# add(5, 3)
+
+# def add(a=0,b=0):
+#     print(a+b)
+# def subtract(a,b):
+#     return a-b
+# result = subtract(10,5)
+# print(result)
+
+# square = lambda x: x*x
+# print(square(4))
+
+#num = [2,8,1,0]
+# num.append(3)
+# print(num)
+
+# num.clear()
+# print(num)
+
+#fruits = ['apple','banana','cherry','apple']
+# print(fruits.count('apple'))
+
+# vegs = ['carrot','tomato','onion']
+# fruits.extend(vegs)
+# print(fruits)
+
+# names=['anusha','sunny','cherry']
+# print(names.index('anusha'))
+
+# names.insert(2,'bunny')
+# print(names)
+
+# fruits.pop(3)
+# print(fruits)
+
+# fruits.remove('tomato')
+# print(fruits)
+
+# fruits.reverse()
+# print(fruits)
+ 
+#colors = ("red", "green", "blue")
+# single = (10,)
+#print(colors[0])
+
+# user = {
+#     "name" : "anusha",
+#     "email":"anushavubba@gmail.com",
+#     "pho no":9392106235
+# }
+# print(user["email"])
+# print(user.get("pho no"))
+
+# user["pho no"]= 8247686795
+# user["address"]="andhra"
+
+# student = {
+#     "name": "Aman",
+#     "age": 20,
+#     "course": "Python"
+# }
+
+# #accessing the values
+# print(student["name"])
+# print(student.get("age"))
+
+# # getting the values
+# student["age"] = 21
+# student["city"] = "Delhi"
+
+# student = {
+#     "name": "Anusha",
+#     "age": 21,
+#     "course": "Python",
+#     "marks": 85
+# }
+# print(student.keys())   #dict_keys(['name', 'age', 'course', 'marks'])
+
+# print(student.values()) #dict_values(['Anusha', 21, 'Python', 85])
+
+# print(student.items())  #dict_items([('name', 'Anusha'), ('age', 21), ('course', 'Python'), ('marks', 85)])
+
+# student.pop("age")
+# print(student)          #{'name': 'Anusha', 'course': 'Python', 'marks': 85}
+
+# student.update({"age": 22})
+# print(student)          #{'name': 'Anusha', 'course': 'Python', 'marks': 85, 'age': 22}
+
+
+a = {1,2,3,4}
+b = {4,5,6,7,8}
+print(a.intersection(b))
+
+
+
+
+
+
+
+
+
